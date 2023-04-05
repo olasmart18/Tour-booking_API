197f9059daf70a7fd5f06f56138de3b1bba196ac
Olasmart-Tech <eweleyeolamide10@gmail.com>
2023-04-05 13:11:45 -0700

    express route created

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const connect = require('./config/db');
+const route = require('./route/tourRoutes');
 
 const port = process.env.PORT || 3030;
 const app = express();
+app.use('/', route);
 
 connect();
 app.listen(port, () => console.log(`server running on port ${port}`));
