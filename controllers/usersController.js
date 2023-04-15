7086d447f6fc16fe7643e054af8b494de511b540
Olasmart-Tech <eweleyeolamide10@gmail.com>
2023-04-15 14:08:55 -0700

    get a single user created

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -17,3 +17,38 @@ exports.createUser = async (req, res) => {
     });
   }
 };
+
+// get all user
+exports.getAllUser = async (req, res) => {
+  try {
+    const foundUsers = await User.find();
+    res.status(200).json({
+      success: true,
+      message: 'users found',
+      data: foundUsers
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: 'not found'
+    });
+  }
+};
+
+// get single user by id
+exports.getSingleUser = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const foundUser = await User.findById(userId);
+    res.status(200).json({
+      success: true,
+      message: 'successful',
+      data: foundUser
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: 'user not found'
+    });
+  }
+};
